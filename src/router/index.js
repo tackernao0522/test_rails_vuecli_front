246501f261c814e555b0da58d27e41620437afc9
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-08 13:24:59 +0900

    Create Vue.js App

---
+++ src/router/index.js
@@ -0,0 +1,67 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import WelcomePage from '../views/WelcomePage'
+import ChatroomPage from '../views/ChatroomPage'
+import useValidate from '../auth/validate'
+
+const { error, validate } = useValidate()
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'Welcome' })
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: WelcomePage,
+    beforeEnter: noRequireAuth
+  },
+  {
+   path: '/chatroom',
+   name: 'Chatroom',
+   component: ChatroomPage,
+   beforeEnter: requireAuth
+ }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/ChatroomPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <NavbarPage />
+    <ChatWindow @connectCable="connectCable" :messages="formattedMessages" ref="chatWindow" />
+    <NewChatForm @connectCable="connectCable" />
+  </div>
+</template>
+
+<script>
+import NavbarPage from '../components/NavbarPage'
+import ChatWindow from '../components/ChatWindow'
+import NewChatForm from '../components/NewChatForm'
+import axios from 'axios'
+import ActionCable from 'actioncable'
+import { formatDistanceToNow } from 'date-fns'
+import { ja } from 'date-fns/locale'
+
+export default {
+  components: { NavbarPage, ChatWindow, NewChatForm },
+  data () {
+    return {
+      messages: [],
+    }
+  },
+  computed: {
+    formattedMessages () {
+      if (!this.messages.length) { return [] }
+      return this.messages.map(message => {
+        let time = formatDistanceToNow(new Date(message.created_at), { locale: ja })
+        return { ...message, created_at: time }
+      })
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    connectCable (message) {
+      this.messageChannel.perform('receive', {
+        message: message,
+        email: window.localStorage.getItem('uid')
+      })
+    }
+  },
+  mounted() {
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+    this.messageChannel = cable.subscriptions.create('RoomChannel', {
+      connected: () => {
+        this.getMessages().then(() => {
+          this.$refs.chatWindow.scrollToBottom()
+        })
+      },
+      received: () => {
+        this.getMessages().then(() => {
+          this.$refs.chatWindow.scrollToBottom()
+        })
+      }
+    })
+  },
+  beforeUnmount () {
+    this.messageChannel.unsubscribe()
+  }
+}
+</script>
+
+<style scoped>
+</style>
